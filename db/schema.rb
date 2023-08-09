1dd19d6adbc03715665ef51dac40cc22e01ab7f0
Wahome <vinwahome@gmail.com>
2023-08-09 17:23:51 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_071639) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_103353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "charities", force: :cascade do |t|
+    t.string "name"
+    t.integer "license"
+    t.integer "description"
+    t.string "email"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
