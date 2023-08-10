9c0132de923845c7db3f487fa4aebc449f749f50
Wahome <vinwahome@gmail.com>
2023-08-10 18:39:15 +0300

    Implement form for creating charities

--- client/src/createcharity.js
+++ client/src/createcharity.js
@@ -1,8 +1,51 @@
 import './createcharities.css'
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
-function Createcharity(){
+function Createcharity({user_id}){
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [image, setImage] = useState('');
+  const [email, setEmail] = useState('');
+  const [license, setLicense] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    const newCharity = {
+      name,
+      description,
+      image,
+      email,
+      license,
+      user_id
+    };
+  
+    fetch('/charities', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCharity),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data) {
+        alert("Post created successfully");
+        navigate("/charities")
+      }
+    })
+    .catch((error) => {
+      setError(error.message);
+    });
+  }
+  
+
+
     return(
         <>
             <section class="vh-100 gradient-custom">
@@ -12,13 +55,20 @@ function Createcharity(){
         <div class="card shadow-2-strong card-registration" style={{borderRadius: '15px'}}>
           <div class="card-body p-4 p-md-5">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Charity Registration Form</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
 
               <div class="row">
                 <div class="col-md-6 mb-4">
 
                   <div class="form-outline">
-                    <input type="text" id="firstName" class="form-control form-control-lg" />
+                    <input 
+                    type="text" 
+                    id="firstName" 
+                    class="form-control form-control-lg"
+                    placeholder='Enter your charity name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
                     <label class="form-label" for="firstName">Charity Name</label>
                   </div>
 
@@ -26,8 +76,15 @@ function Createcharity(){
                 <div class="col-md-6 mb-4">
 
                   <div class="form-outline">
-                    <input type="text" id="lastName" class="form-control form-control-lg" />
-                    <label class="form-label" for="lastName">Registration No.</label>
+                    <input 
+                    type="text" 
+                    id="lastName" 
+                    class="form-control form-control-lg" 
+                    placeholder='Enter your License No.'
+                    value={license}
+                    onChange={(e) => setLicense(e.target.value)}
+                    />
+                    <label class="form-label" for="lastName">License No.</label>
                   </div>
 
                 </div>
@@ -37,8 +94,13 @@ function Createcharity(){
                 <div class="col-md-6 mb-4 d-flex align-items-center">
 
                   <div class="form-outline datepicker w-100">
-                    <input type="text" class="form-control form-control-lg" id="birthdayDate" />
-                    <label for="birthdayDate" class="form-label">License No.</label>
+                    <input 
+                    type="email" 
+                    class="form-control form-control-lg" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                     />
+                    <label for="birthdayDate" class="form-label">Email address</label>
                   </div>
 
                 </div>
@@ -68,40 +130,39 @@ function Createcharity(){
               </div>
 
               <div class="row">
-                <div class="col-md-6 mb-4 pb-2">
+                <div class="col-md-14 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="email" id="emailAddress" class="form-control form-control-lg" />
-                    <label class="form-label" for="emailAddress">Email</label>
+                    <input 
+                    type="text" 
+                    class="form-control form-control-lg" 
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)} 
+                    />
+                    <label class="form-label" for="emailAddress">Image URL</label>
                   </div>
 
                 </div>
-                <div class="col-md-6 mb-4 pb-2">
+                <div class="col-md-14 mb-4 pb-2">
 
                   <div class="form-outline">
-                    <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
-                    <label class="form-label" for="phoneNumber">Address</label>
+                    <input 
+                    type="text" 
+          
+                    class="form-control form-control-lg" 
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <label class="form-label" for="description">Description</label>
                   </div>
 
                 </div>
               </div>
 
-              {/* <div class="row">
-                <div class="col-12">
-
-                  <select class="select form-control-lg">
-                    <option value="1" disabled>Choose option</option>
-                    <option value="2">Subject 1</option>
-                    <option value="3">Subject 2</option>
-                    <option value="4">Subject 3</option>
-                  </select>
-                  <label class="form-label select-label">Choose option</label>
-
-                </div>
-              </div> */}
-
               <div class="mt-4 pt-2">
-                <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
+                
+                
+                 <button class="btn btn-primary btn-lg" type="submit" value="Submit">Submit</button>  
               </div>
 
             </form>
